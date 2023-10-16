69c3162855ed805c8318f2cfaea0b6e539821c6a
varya <varya@users.noreply.github.com>
2023-10-16 17:05:59 +0000

    deploy: ce0839c3d0206ae361591eb00925eb466f9b36a8

---
+++ _next/static/LEitWBt38tLxPsN3_jUSb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-1b6126bd64c2655abd44.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-1994679f6d1c98127199.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
